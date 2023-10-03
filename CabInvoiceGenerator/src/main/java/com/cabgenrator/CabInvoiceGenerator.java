c7a7575952d7172e715e057dcc2f801a188aefba
KadamTeju <tejushinde8897@gmail.com>
2023-10-03 16:56:30 +0530

    UC1 Complete.

--- CabInvoiceGenerator/src/main/java/com/cabgenrator/CabInvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/cabgenrator/CabInvoiceGenerator.java
@@ -1,5 +1,16 @@
 package com.cabgenrator;
 
 public class CabInvoiceGenerator {
-
+    public final double costperkm=10.0;
+    double costpermin=1.0;
+    double min_fare =5;
+    public double calculateFare(double distance,int time)
+    {
+        double totalfare=(distance+costperkm)+(time*costpermin);
+        if(totalfare<5)
+        {
+            return min_fare;
+        }
+        return totalfare;
+    }
 }
